6175d80ddc37007fb98045c8e3074dad16ca7ec6
dalyzhee <dalmasodhiambolevi@gmail.com>
2020-07-22 00:58:25 +0300

    first commit

---
+++ scraper.py
@@ -0,0 +1,22 @@
+import requests
+from bs4 import BeautifulSoup as bs
+import csv
+
+url = 'https://report.boonecountymo.org/mrcjava/servlet/RMS01_MP.I00030s?max_rows=500'
+response = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
+html = response.content
+
+soup = bs(html)
+table = soup.find('tbody', attrs={'class': 'stripe'})
+list_of_rows = []
+for row in table.findAll('tr'):
+	list_of_cells = []
+	for cell in row.findAll('td'):
+		text = cell.text.replace('&nbsp;', '')
+		list_of_cells.append(text)
+	list_of_rows.append(list_of_cells)
+
+outfile = open("./inmates2.csv", "w")
+writer = csv.writer(outfile)
+writer.writerow(["Last", "First", "Middle", "No", "Gender", "Race", "Age", "City", "State"])
+writer.writerows(list_of_rows)
